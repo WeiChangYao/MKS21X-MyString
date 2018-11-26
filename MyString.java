7665e44935c9b7dd69e9edd570d3568b3d93b05f
weichanhyao <wyao1@stuy.edu>
2018-11-26 00:35:01 -0500

    added better comments

--- MyString.java
+++ MyString.java
@@ -1,23 +1,24 @@
 public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
-  public MyString(CharSequence s){ //gets chars and puts it in data
+  public MyString(CharSequence s){       
+    char[] data = new char[s.length]      //data is as long as the sequence
     for (int i = 0; i < s.length(); i++){
-      data[i] = s.charAt(i);
+      data[i] = s.charAt(i);              //gets chars and puts it in data
     }
   }
   public String toString{ 
     String myStringString
     for (int i = 0; i < data.length; i++){
-      myStringString += "" + data[i];
+      myStringString += "" + data[i];       //adds thing in data
       if (i + 1 < data.length){
-        myStringString += ", ";
+        myStringString += ", ";             //adds a comma as long as it's not the last character
       }
     }
   }
   public int length(){
-    int size;
-    for (int i = 0; i < data.length; i ++){ //counts number of stuff in data
-      size++;
+    int size;           
+    for (int i = 0; i < data.length; i ++){ //makes int size and adds one everytime it goes through a character
+      size++;                               
     }
     return size;
   }
