74c7705745e6fd3e7a87f82dd2bff913392cd9d6
weichanhyao <wyao1@stuy.edu>
2018-11-26 01:39:57 -0500

    added everything and fixed everything

--- MyString.java
+++ MyString.java
@@ -7,24 +7,25 @@ public class MyString implements CharSequence{ //,Comparable<CharSequence>{
     }
   }
   public String toString(){ 
-    String myStringString;
+    String myStringString = "";
     for (int i = 0; i < data.length; i++){
       myStringString += "" + data[i];       //adds thing in data
       if (i + 1 < data.length){
         myStringString += ", ";             //adds a comma as long as it's not the last character
       }
     }
+    return myStringString;
   }
   public int length(){
-    int size;           
+    int size = 0;           
     for (int i = 0; i < data.length; i ++){ //makes int size and adds one everytime it goes through a character
       size++;                               
     }
     return size;
   }
   public CharSequence subSequence (int start, int end){
-    CharSequence a = "";                    //makes a new charsqeuence?
-    for (int i = start; i < end; i ++){     //adds what ever is in data to the new sequence
+    String a = "";                    //makes a new charsqeuence?
+    for (int i = start; i < end; i ++){     //adds what is in data to the new sequence
       a = a + data[i];                       
     }
     return a;                               //returns new sequence
